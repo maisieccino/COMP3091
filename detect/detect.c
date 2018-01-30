d89970ce523672b2c115c4dbf0e6cfd614f9b64a
Matthew Bell <m.bell@cs.ucl.ac.uk>
2018-01-30 00:44:20 +0000

    [detect] 1st attempt at motion click interrupt

--- detect/detect.c
+++ detect/detect.c
@@ -5,6 +5,8 @@
 #include <sys/clock.h>
 #include "letmecreate/core/network.h"
 #include "letmecreate/core/common.h"
+#include "letmecreate/click/motion.h"
+#include "letmecreate/core/debug.h"
 #include <sys/etimer.h>
 
 #include <string.h>
@@ -25,16 +27,27 @@
 #define PAIR_ID -1
 #endif
 
+static bool motion_detected;
+
+void motion_callback(uint8_t event)
+{
+  motion_detected = true;
+}
+
 PROCESS(detect_main, "Main process for detector");
 AUTOSTART_PROCESSES(&detect_main);
 PROCESS_THREAD(detect_main, ev, data)
 {
   PROCESS_BEGIN();
+  INIT_NETWORK_DEBUG();
   {
     static struct etimer et;
     static struct uip_udp_conn *connection;
     static char buf[BUFFER_SIZE];
+    PRINTF("Hello\n");
+
     connection = udp_new_connection(CLIENT_PORT, SERVER_PORT, UDP_CONNECTION_ADDR);
+    motion_detected = false;
 
     // wait a bit to try to give the UDP connection a chance... might work?
     etimer_set(&et, CLOCK_SECOND * 20);
@@ -49,9 +62,20 @@ PROCESS_THREAD(detect_main, ev, data)
       }
     }
 
+    motion_click_enable(MIKROBUS_1);
+    motion_click_attach_callback(MIKROBUS_1, motion_callback);
+
     etimer_set(&et, PROC_INTERVAL);
     while (true) {
       PROCESS_YIELD();
+      if (motion_detected) {
+        sprintf(buf, "{\"device_id\":%d,\"command\":\"motion\"}", DEVICE_ID);
+        udp_packet_send(connection, buf, strlen(buf));
+        PROCESS_WAIT_UDP_SENT();
+        motion_detected = false;
+        continue;
+      }
+
       if (etimer_expired(&et)) {
         sprintf(buf, "{\"device_id\":%d,\"command\":\"heartbeat\"}", DEVICE_ID);
         udp_packet_send(connection, buf, strlen(buf));
@@ -59,6 +83,8 @@ PROCESS_THREAD(detect_main, ev, data)
         etimer_restart(&et);
       }
     }
+
+    motion_click_disable(MIKROBUS_1);
   }
   PROCESS_END();
 }
