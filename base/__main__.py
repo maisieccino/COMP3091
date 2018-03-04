ece221fdd0ef6fde991fbdf9e76adda7248d0df0
Matthew Bell <m.bell@cs.ucl.ac.uk>
2018-03-04 21:57:08 +0000

    [base] Even better error handling

--- base/__main__.py
+++ base/__main__.py
@@ -13,32 +13,51 @@ listen_sock.bind(("::", 9876))
 
 device_pairs = {}
 
+# Maps device ids to their pair ids
+device_ids_to_pairs = {}
 
-def command_id(obj):
+
+def command_id(obj, conn):
     try:
         pair_id = obj["pair_id"]
-    except KeyError:
-        print("no pair_id provided")
-        return
-    try:
         device_id = obj["device_id"]
-    except KeyError:
-        print("no device_id provided")
-        return
-    try:
         device_type = obj["type"]
     except KeyError:
-        print("no type provided")
+        print("missing keys. make sure `pair_id`, `device_id` and `type` are provided.")
         return
     print("device {} to be paired in pair {}".format(device_id, pair_id))
     if not pair_id in device_pairs:
         device_pairs[pair_id] = {}
+    if not device_id in device_ids_to_pairs:
+        device_ids_to_pairs[device_id] = device_id
     device_pairs[pair_id][device_type] = device_id
     print("device pair {}: now {}".format(pair_id, device_pairs[pair_id]))
 
 
-def command_heartbeat(obj):
+def command_heartbeat(obj, conn):
     print("device {} checking in".format(obj["device_id"]))
+
+
+def command_motion(obj, conn):
+    try:
+        device_id = obj["device_id"]
+    except KeyError:
+        print("Missing device_id")
+        return
+    print("registered motion on device {}".format(device_id))
+    if device_id in device_ids_to_pairs:
+        pair_id = device_ids_to_pairs[device_id]
+        if not "camera" in device_pairs[pair_id]:
+            print("No camera associated with this pair. no further action.")
+            return
+        camera_id = device_pairs[pair_id]["camera"]
+        print("sending capture command to device {}".format(camera_id))
+        return
+
+
+def command_unknown(obj, conn):
+    print("Unknown command `{}`".format(obj["command"]))
+    conn.send("what?!\n".encode())
 
 
 def quit_handler(signal, frame):
@@ -52,31 +71,32 @@ listen_sock.listen(1)
 
 while True:
     print('Waiting for a connection...')
-    connection, client_address = listen_sock.accept()
+    conn, client_address = listen_sock.accept()
     try:
         print("Connection from {}".format(client_address))
         while True:
-            data = connection.recv(1024)
+            data = conn.recv(1024)
             try:
                 obj = json.loads(data.decode("ascii"))
                 print(obj)
                 command_switch = {
                     "id": command_id,
-                    "heartbeat": command_heartbeat
+                    "heartbeat": command_heartbeat,
+                    "motion": command_motion
                 }
-                func = command_switch.get(
-                    obj["command"], lambda: "Unknown command")
-                func(obj)
+                func = command_switch.get(obj["command"], command_unknown)
+                func(obj, conn)
             except json.JSONDecodeError:
                 if len(data) == 0:
                     break
                 print("RX [{}] {}".format(datetime.utcnow().isoformat(), data))
             except:
                 print("Some random error: {}".format(sys.exc_info()[0]))
+                raise
         else:
             print("No more data")
             break
     except ConnectionResetError:
         print("Connection lost.")
     finally:
-        connection.close()
+        conn.close()
