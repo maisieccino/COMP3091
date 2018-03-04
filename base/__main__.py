0661f55d0c2efa8fdf738746fd088c15fd120878
Matthew Bell <m.bell@cs.ucl.ac.uk>
2018-03-04 21:08:31 +0000

    [base] improve error detection, fix pairing code

--- base/__main__.py
+++ base/__main__.py
@@ -15,12 +15,25 @@ device_pairs = {}
 
 
 def command_id(obj):
-    pair_id = obj["pair_id"]
-    device_id = obj["device_id"]
+    try:
+        pair_id = obj["pair_id"]
+    except KeyError:
+        print("no pair_id provided")
+        return
+    try:
+        device_id = obj["device_id"]
+    except KeyError:
+        print("no device_id provided")
+        return
+    try:
+        device_type = obj["type"]
+    except KeyError:
+        print("no type provided")
+        return
     print("device {} to be paired in pair {}".format(device_id, pair_id))
-    if not device_id in device_pairs:
+    if not pair_id in device_pairs:
         device_pairs[pair_id] = {}
-    device_pairs[pair_id][obj["type"]] = device_id
+    device_pairs[pair_id][device_type] = device_id
     print("device pair {}: now {}".format(pair_id, device_pairs[pair_id]))
 
 
@@ -39,11 +52,11 @@ listen_sock.listen(1)
 
 while True:
     print('Waiting for a connection...')
-    connection, client_address = sock.accept()
+    connection, client_address = listen_sock.accept()
     try:
         print("Connection from {}".format(client_address))
         while True:
-            data, addr_info = connection.recv(1024)
+            data = connection.recv(1024)
             try:
                 obj = json.loads(data.decode("ascii"))
                 print(obj)
@@ -55,7 +68,11 @@ while True:
                     obj["command"], lambda: "Unknown command")
                 func(obj)
             except json.JSONDecodeError:
+                if len(data) == 0:
+                    break
                 print("RX [{}] {}".format(datetime.utcnow().isoformat(), data))
+            except:
+                print("Some random error: {}".format(sys.exc_info()[0]))
         else:
             print("No more data")
             break
